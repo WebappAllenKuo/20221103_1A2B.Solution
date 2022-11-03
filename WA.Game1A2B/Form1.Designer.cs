8024a8a4984728eb81dc0dd1e1cd6201bb31d3be
allen <g01.webapp@gmail.com>
2022-11-03 09:14:37 +0800

    建立 UI及程式

--- WA.Game1A2B/Form1.Designer.cs
+++ WA.Game1A2B/Form1.Designer.cs
@@ -28,13 +28,82 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.newgameButton = new System.Windows.Forms.Button();
+			this.answerLabel = new System.Windows.Forms.Label();
+			this.historyTextBox = new System.Windows.Forms.TextBox();
+			this.inputTextBox = new System.Windows.Forms.TextBox();
+			this.playButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// newgameButton
+			// 
+			this.newgameButton.Location = new System.Drawing.Point(52, 26);
+			this.newgameButton.Name = "newgameButton";
+			this.newgameButton.Size = new System.Drawing.Size(100, 23);
+			this.newgameButton.TabIndex = 0;
+			this.newgameButton.Text = "New Game";
+			this.newgameButton.UseVisualStyleBackColor = true;
+			this.newgameButton.Click += new System.EventHandler(this.newgameButton_Click);
+			// 
+			// answerLabel
+			// 
+			this.answerLabel.AutoSize = true;
+			this.answerLabel.Location = new System.Drawing.Point(203, 30);
+			this.answerLabel.Name = "answerLabel";
+			this.answerLabel.Size = new System.Drawing.Size(78, 15);
+			this.answerLabel.TabIndex = 1;
+			this.answerLabel.Text = "answerLabel";
+			// 
+			// historyTextBox
+			// 
+			this.historyTextBox.Location = new System.Drawing.Point(52, 130);
+			this.historyTextBox.Multiline = true;
+			this.historyTextBox.Name = "historyTextBox";
+			this.historyTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.historyTextBox.Size = new System.Drawing.Size(240, 262);
+			this.historyTextBox.TabIndex = 2;
+			// 
+			// inputTextBox
+			// 
+			this.inputTextBox.Location = new System.Drawing.Point(52, 79);
+			this.inputTextBox.Name = "inputTextBox";
+			this.inputTextBox.Size = new System.Drawing.Size(148, 25);
+			this.inputTextBox.TabIndex = 3;
+			// 
+			// playButton
+			// 
+			this.playButton.Location = new System.Drawing.Point(206, 78);
+			this.playButton.Name = "playButton";
+			this.playButton.Size = new System.Drawing.Size(75, 23);
+			this.playButton.TabIndex = 4;
+			this.playButton.Text = "Play";
+			this.playButton.UseVisualStyleBackColor = true;
+			this.playButton.Click += new System.EventHandler(this.playButton_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(358, 450);
+			this.Controls.Add(this.playButton);
+			this.Controls.Add(this.inputTextBox);
+			this.Controls.Add(this.historyTextBox);
+			this.Controls.Add(this.answerLabel);
+			this.Controls.Add(this.newgameButton);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button newgameButton;
+		private System.Windows.Forms.Label answerLabel;
+		private System.Windows.Forms.TextBox historyTextBox;
+		private System.Windows.Forms.TextBox inputTextBox;
+		private System.Windows.Forms.Button playButton;
 	}
 }
 
--- WA.Game1A2B/Form1.cs
+++ WA.Game1A2B/Form1.cs
@@ -12,9 +12,38 @@ namespace WA.Game1A2B
 {
 	public partial class Form1 : Form
 	{
+		private Game game;
+
 		public Form1()
 		{
 			InitializeComponent();
+
+			answerLabel.Text = string.Empty;
+			inputTextBox.MaxLength = 4;
+		}
+
+		private void newgameButton_Click(object sender, EventArgs e)
+		{
+			game = new Game("1234");
+			answerLabel.Text = game.Answer; // 用label顯示答案,方便確認程式運作正確性
+		}
+
+		private void playButton_Click(object sender, EventArgs e)
+		{
+			string input = inputTextBox.Text;
+
+			// todo 檢查必需輸入四個不同的數字
+
+			string result = game.Play(input);
+
+			if (result == "4A0B")
+			{
+				MessageBox.Show("您答對了");
+				return;
+			}
+			
+			//若答錯,將結果加到最下方
+			historyTextBox.Text += result + "\r\n";
 		}
 	}
 }
